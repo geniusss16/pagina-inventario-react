ebbe3fd80856add724c12bd5ee044750b35e0bda
geniusss16 <alfredo.60.faria@gmail.com>
2022-09-15 01:11:57 -0400

    progreso con convetidor

--- src/assets/components/Convertidor.jsx
+++ src/assets/components/Convertidor.jsx
@@ -4,16 +4,23 @@ import "../hojas-de-estilos/Convertidor.css"
 export const Convertidor = () => {
   return (
     <>
-    <div className='d-flex justify-content-center p-5'>
- <div className='toto' >
-    <select  name="sel1" id="sellist1" className="form-select  w-100 ">
+ <div className='d-flex justify-content-center '>
+  <div className='row toto' >
+    <img className='col-12 mt-4' src="../public/imagenes/dolar.jpg" alt="dolarlogo" />
+    <select  name="sel1" id="sellist1" className=" col-1 form-select mt-4  w-100 ">
         <option value="1">DOLARTODAY</option>
         <option value="2">BCV</option>
         <option value="3">PERSONALIZADA</option>
     </select>
-         <label className="label d-flex flex-row-reverse p-2"> 1</label>
+         <label type='number' className=" colorgry col-12  label d-flex p-2"> Cantidad:</label>
+         <input type="text" class="form-control" id="input1" name="input1"></input>
+         <label type='number' className=" colorgry col-12  label d-flex p-2">Resultados en:</label>
+         <input type="text" class="form-control" id="input1" name="input1"></input>
+         <div className="d-flex justify-content-center">
+           <button type="button" className="btn btn-success mt-3 col-4"><i class="bi bi-trash"></i></button>
+         </div>   
    </div>
-    </div>
+ </div>
   
     </>
   )
