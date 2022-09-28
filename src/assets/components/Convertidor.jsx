ffb86dbeede808f00f283e3fbee42cb140a7c82e
geniusss16 <alfredo.60.faria@gmail.com>
2022-09-27 22:19:46 -0400

    se afino el convertidor de divisa

--- src/assets/components/Convertidor.jsx
+++ src/assets/components/Convertidor.jsx
@@ -2,50 +2,38 @@ import React from 'react'
 import "../hojas-de-estilos/Convertidor.css"
 import { useState,useEffect } from 'react'
 
-
 export const Convertidor = () => {
+
+  const estadoInicial = (Number(1.00).toFixed(2))
+   
+   const [monto,setMonto] = useState (estadoInicial);
+   const [resultado,setResultado] = useState ("");
 
   const valores = [
    {
     moneda: "usa",
-    valor1: 8.31
+    valor1:  8.42
 
    }
   
   ]
 
- /*  useState((0).toFixed(2))
- */
+  useEffect(() =>{
 
-   /*  valor inicial del input 1 => */
-   const [monto,setMonto] = useState (Number().toFixed(2));
-   const [resultado,setResultado] = useState ("");
+   setResultado(monto * valores[0].valor1)
 
+  },[monto])
 
-   function actualizar () {
-
-    
-   }
-   
   
-    // useEffect(() => {
-    //  console.log(`prueba del primer input ${monto}`) 
-      
-    //  let midorilla = 3
-
-    //  setMonto2( midorilla )
-    //  console.log(`prueba del segundo input ${monto2}`)
-    // }, [monto]);
-
-
    function capturavalor1 (e) {  
-    setMonto(e.target.value)
-    setResultado(monto * valores[0].valor1)
-
+    setMonto((e.target.value))
    }
-    
+
+   const reiniciarBT = () => { setMonto(estadoInicial)}
+   
     console.log(`prueba del primer input ${monto}`) 
     console.log(`prueba del segundo input ${resultado}`) 
+    console.log(`prueba del segundo input ${(resultado).toLocaleString("es-VE", {style:"currency", currency:"VES"})}`) 
 
   return (
     <>
@@ -58,11 +46,11 @@ export const Convertidor = () => {
         <option value="3">PERSONALIZADA</option>
     </select>
          <label type="text" className="colorgry col-12  label d-flex p-2"> Cantidad:</label>
-         <input type="number" className="form-control text-right" id="input1" name="input1" step="0.01" onChange={capturavalor1} ></input>
+         <input type="number" className="form-control text-right" id="input1" name="input1"  onChange={capturavalor1} value={(monto)}></input>
          <label type="text" className=" colorgry col-12  label d-flex p-2">Resultados en:</label>
-         <input type="number" className="form-control text-right " id="input1" name="input1"readOnly={true} value={Number(resultado).toFixed(2)}></input>
+         <input type="text" className="form-control text-right " id="input2" name="input2"readOnly={true} value={(resultado).toLocaleString("es-VE", {style:"currency", currency:"VES"})}></input>
          <div className="d-flex justify-content-center">
-           <button type="button" className="btn btn-success mt-3 col-4"><i className="bi bi-trash"></i></button>
+           <button type="button" className="btn btn-success mt-3 col-4"onClick={reiniciarBT}><i className="bi bi-trash" ></i></button>
          </div>   
    </div>
  </div>
